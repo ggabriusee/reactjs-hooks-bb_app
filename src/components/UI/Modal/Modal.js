7e40bb769b27124562e952549f5634bc2fbf9514
ggabriuss <ggabriusee@gmail.com>
2020-12-15 22:33:38 +0200

    converting burgIngr, orderSumm, Modal with useMemo; added custom hook

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
+const modal = props => {
+  //component only update if this.props.show changes
+  //we don't upadate(re-render) OrderSummary (which is in props.children)
+  //when it is not showned on the screen
+  //children check is so that the component will update when it does get new children
+  // shouldComponentUpdate ( nextProps, nextState ) {
+  //     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+  // }
 
-    //component only update if this.props.show changes
-    //we don't upadate(re-render) OrderSummary (which is in props.children)
-    //when it is not showned on the screen
-    //children check is so that the component will update when it does get new children
-    shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
-    componentWillUpdate () {
-        console.log('[Modal] WillUpdate');
-    }
-
-    render () {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        )
-    }
-}
-
-export default Modal;
+//React.memo makes so that the component updates only when its props change
+//second arg. is a custom comparison function
+//previously (look up) we checked if we want to continue which was when props not equal
+//now we need to return if they are equal
+export default React.memo(
+  modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
